fb0cf2631662e69946624a17eae89990fe9cab12
Victor Campos <victor.cmp93@gmail.com>
2023-12-07 17:06:47 +0100

    routes and pages

--- vegolution/src/App.jsx
+++ vegolution/src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
-  
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/register" element={<Register />}/>
+      <Route path="/contact" element={<Contact />}/>
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
     </>
   )
 }
