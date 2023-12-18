5d4100fe7a9be3d5dc56b2d4d5ac07daa3bb1e69
Aramendiander <aramendiander@gmail.com>
2023-12-18 16:36:23 +0100

    basic cart view

--- vegolution/src/pages/Cart.jsx
+++ vegolution/src/pages/Cart.jsx
@@ -1,14 +1,14 @@
 import { React, useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 import Layout from '../components/Layout'
 
 const Cart = () => {
     const [userId, setUserId] = useState('')
     const [cart, setCart] = useState([])
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        console.log(document.cookie)
-        console.log(document)
-        console.log('hola')
         fetchCart()
     }, [])
 
@@ -16,10 +16,26 @@ const Cart = () => {
 
     const fetchCart = async () => {
         try {
-            const response = await fetch(`http://localhost:3006/cart/?userId=65774cfc915b33dfea928eb4`, { credentials: 'include' });
+            const response = await fetch(`http://localhost:3006/cart/`, { credentials: 'include' });
+            if (response.status === 401) {
+                try {
+                    console.log('hopla')
+                    return window.location.href = "/login";
+                }
+                catch (e) {
+                    console.log(e)
+                }
+            }
+
+            if (response.status === 404) {
+                setError('Not found');
+                return error;
+            }
+
             const data = await response.json();
-            setCart(data)
-            console.log(data)
+            setCart(data.userCart)
+            setLoading(false);
+
         }
         catch (e) {
             console.log('Error fetching data')
@@ -27,13 +43,28 @@ const Cart = () => {
     }
 
 
-
-
+    console.log(cart)
 
 
     return (
         <Layout>
-            <h1>Cart</h1>
+            {loading ? null : (
+                <>
+                    <h1>Cart</h1>
+                    <div>
+                        {cart.map((item) => {
+                            return (
+                                <div className="cartitem" key={item._id}>
+                                    <h2>{item.product.name}</h2>
+                                    <img src={item.product.picture} alt={item.product.name} />
+                                    <p>{item.product.price}</p>
+                                    <p>{item.product.quantity}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </>
+            )}
         </Layout>
     )
 }
