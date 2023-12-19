aef3e036c8911077fe63b0ae2a7cef34e1b3c971
Aramendiander <aramendiander@gmail.com>
2023-12-19 01:33:39 +0100

    cart functions working, cart history basic approach

--- vegolution/src/pages/Cart.jsx
+++ vegolution/src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react'
-import { Navigate } from 'react-router-dom'
+import { Navigate, Link } from 'react-router-dom'
 import Layout from '../components/Layout'
 
 const Cart = () => {
@@ -7,9 +7,13 @@ const Cart = () => {
     const [cart, setCart] = useState([])
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(true)
+    const [totalPrice, setTotalPrice] = useState(0)
+
 
     useEffect(() => {
         fetchCart()
+        const body = document.querySelector('body');
+        body.classList.add('cart_page');
     }, [])
 
 
@@ -19,7 +23,6 @@ const Cart = () => {
             const response = await fetch(`http://localhost:3006/cart/`, { credentials: 'include' });
             if (response.status === 401) {
                 try {
-                    console.log('hopla')
                     return window.location.href = "/login";
                 }
                 catch (e) {
@@ -34,8 +37,14 @@ const Cart = () => {
 
             const data = await response.json();
             setCart(data.userCart)
+            console.log(data.userCart)
+            let priceCalc = 0
+            data.userCart.map((item) => {
+                let itemPrice = item.product.price * item.quantity
+                priceCalc += itemPrice;
+            })
+            setTotalPrice(priceCalc)
             setLoading(false);
-
         }
         catch (e) {
             console.log('Error fetching data')
@@ -43,25 +52,88 @@ const Cart = () => {
     }
 
 
-    console.log(cart)
+    const deleteFromCart = async (productId) => {
+        try {
+            const response = await fetch('http://localhost:3006/cart/delete-from-cart', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ productId }),
+                credentials: 'include',
+            });
+
+            if (!response.ok) {
+                throw new Error('Error deleting product from cart');
+            }
+            fetchCart();
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+
+    const purchaseCart = async () => {
+        try {
+            const response = await fetch('http://localhost:3006/cart/purchase', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+
+            if (!response.ok) {
+                throw new Error('Error purchasing cart');
+            }
+            fetchCart();
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 
 
     return (
         <Layout>
             {loading ? null : (
                 <>
-                    <h1>Cart</h1>
-                    <div>
-                        {cart.map((item) => {
-                            return (
-                                <div className="cartitem" key={item._id}>
-                                    <h2>{item.product.name}</h2>
-                                    <img src={item.product.picture} alt={item.product.name} />
-                                    <p>{item.product.price}</p>
-                                    <p>{item.product.quantity}</p>
-                                </div>
-                            )
-                        })}
+                    <Link to='/cart/history'>History</Link>
+                    <div className='cart'>
+                        <div className='cartitems'>
+                            <h2>Cart</h2>
+                            {cart.map((item) => {
+                                let itemPrice = item.product.price * item.quantity
+                                return (
+
+                                    <div className="cartitem" key={item._id}>
+                                        <img src={item.product.picture} alt={item.product.name} />
+                                        <div className="cartitem_name">
+                                            <p>Product:</p>
+                                            <p>{item.product.name}</p>
+                                        </div>
+                                        <div>
+                                            <p>Total price:</p>
+                                            <p>{item.product.price}€</p>
+
+                                        </div>
+                                        <div>
+                                            <p>Quantity</p>
+                                            <p>{item.quantity}</p>
+                                        </div>
+                                        <div>
+                                            <button onClick={() => deleteFromCart(item.product._id)}>Delete</button>
+                                        </div>
+                                    </div>
+
+                                )
+                            })}
+                        </div>
+                        <div className='checkout'>
+                            <h2>Checkout</h2>
+                            <p>Total:</p>
+                            <p>{totalPrice}€</p>
+                            <button onClick={() => purchaseCart()}>Purchase</button>
+                        </div>
                     </div>
                 </>
             )}
---
+++ vegolution/src/pages/CartHistory.jsx
@@ -0,0 +1,98 @@
+import { React, useState, useEffect } from 'react'
+import Layout from '../components/Layout'
+
+const CartHistory = () => {
+    const [cart, setCart] = useState([])
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [activeComponent, setActiveComponent] = useState('')
+
+
+    useEffect(() => {
+        fetchCart()
+    }, [])
+
+
+    const fetchCart = async () => {
+        try {
+            const response = await fetch(`http://localhost:3006/cart/history`, { credentials: 'include' });
+            if (response.status === 401) {
+                try {
+                    return window.location.href = "/login";
+                }
+                catch (e) {
+                    console.log(e)
+                }
+            }
+
+            if (response.status === 404) {
+                setError('Not found');
+                return error;
+            }
+
+            const data = await response.json();
+            setCart(data)
+            let priceCalc = 0
+            data.userCart.map((item) => {
+                let itemPrice = item.product.price * item.quantity
+                priceCalc += itemPrice;
+            })
+            setTotalPrice(priceCalc)
+            setLoading(false);
+        }
+        catch (e) {
+            console.log('Error fetching data')
+        }
+    }
+
+    const handleHistoryClick = (date) => {
+        setActiveComponent(date)
+    }
+
+
+
+
+
+    console.log(activeComponent)
+
+
+
+
+    return (
+        <Layout>
+            <h1>History as</h1>
+            <div className="cart_history">
+                <div className="single_cart_history">
+                    {cart.map((item) => {
+                        const totalPrice = item.products.reduce((total, product) => total + product.product.price * product.quantity, 0);
+                        return (
+                            <div key={item.date}>
+                                <p onClick={() => { handleHistoryClick(item.date) }}>{item.date}</p>
+                                {activeComponent === item.date &&
+                                    <>
+                                        {item.products.map((item, index) => {
+                                            const product = item.product
+                                            console.log(item)
+                                            return (
+                                                <div className='singlehistory' key={index}>
+                                                    <img src={product.picture} alt={product.name} key={product.picture} />
+                                                    <p key={product.name}>{product.name}</p>
+                                                    <p key={product.price}>{product.price}€</p>
+                                                    <p key={item.quantity}>{item.quantity}</p>
+                                                </div>
+                                            )
+                                        })}
+                                        <p>Total Price: {totalPrice}€</p>
+                                    </>
+                                }
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default CartHistory
